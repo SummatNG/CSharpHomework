67380eb6bffea3a2673e2f60daabb3c574606115
SummatNG <79800346+SummatNG@users.noreply.github.com>
2021-04-28 10:38:56 +0800

    Add files via upload

---
+++ Homework2/GetValue/GetValue/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace GetValue
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[9]{ 2, 4, 87, 45, 90, 24, 88, 66, 287 };
+            Console.WriteLine("当前整数组的最大值为:");
+            Console.WriteLine(GetMax(numbers));
+            Console.WriteLine("当前整数组的最小值为:");
+            Console.WriteLine(GetMin(numbers));
+            Console.WriteLine("当前整数组的和为:");
+            Console.WriteLine(GetSum(numbers));
+            Console.WriteLine("当前整数组的平均值为:");
+            Console.WriteLine(GetAvg(numbers));
+        }
+
+        public static int GetMax(int[] input)
+        {
+            int max = input[0];
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > max)
+                    max = input[i];
+            }
+            return max;
+        }
+
+        public static int GetMin(int[] input)
+        {
+            int min = input[0];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < min)
+                    min = input[i];
+            }
+            return min;
+        }
+
+
+
+        public static int GetSum(int[] input)
+        {
+            int sum = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                sum = sum + input[i];
+            }
+
+            return sum;
+        }
+
+        public static double GetAvg(int[] input)
+        {
+            double avg = (double) GetSum(input) / input.Length;
+            return avg;
+        }
+    }
+}
---
+++ Homework2/PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] result = GetPrimeFactor(input);
+            Console.WriteLine("该整数的质数因子有:");
+            for (int i = 0;i < result.Length; i++)
+            {
+                Console.WriteLine(result[i] + " ");
+            }
+
+            
+        }
+
+        public static int[] GetPrimeFactor(int number)
+        {
+            if (number <= 1)
+                return null;
+            else
+            {
+                int[] primeFactors = new int[0];
+                for (int factor = 2,i = 0; factor * factor <= number; factor++)
+                {
+                    while (number % factor == 0)
+                    {
+                        AddElements(primeFactors, i, factor);
+                        i++;
+                        number = number / factor;
+                    }
+                }
+                return primeFactors;
+
+
+            }
+        }
+
+        public static int[] AddElements(int[] originalArray, int index, int value)
+        {
+            if (index >= originalArray.Length)
+            {
+                index = originalArray.Length;
+            }
+
+            int[] newArray = new int[originalArray.Length + 1];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (index >= 0)
+                {
+                    if (i < index)
+                    {
+                        newArray[i] = originalArray[i];
+                    }
+                    else if (i == index)
+                    {
+                        newArray[i] = value;
+                    }
+                    else
+                    {
+                        newArray[i] = originalArray[i - 1];
+                    }
+                }
+                    
+            }
+            return newArray;
+        }
+    }
+}
+
+
+
+ 
