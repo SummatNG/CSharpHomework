dc2933bde3245f279dc413890738bbc73f3166ae
SummatNG <79800346+SummatNG@users.noreply.github.com>
2021-04-28 10:40:48 +0800

    Add files via upload

---
+++ Homework4/Clock/Clock/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+
+        public delegate void ClockHandler(ClockEventArgs args);
+
+        public class ClockEventArgs
+        {
+            private DateTime currentTime;
+            private DateTime alarmTime;
+
+            public DateTime CurrentTime
+            {
+                get
+                {
+                    return currentTime;
+                }
+                set
+                {
+                    currentTime = value;
+                }
+            }
+            public DateTime AlarmTime
+            {
+                get
+                {
+                    return alarmTime;
+                }
+                set
+                {
+                    alarmTime = value;
+                }
+            }
+        }
+
+        public class ClockEvent
+        {
+            private bool isPause = true;
+            public event ClockHandler Tick;
+            public event ClockHandler Alarm;
+
+            public void ShowTime(DateTime alarmColck)
+            {
+                ClockEventArgs args = new ClockEventArgs();
+                args.AlarmTime = alarmColck;
+                while (true)
+                {
+                    args.CurrentTime = System.DateTime.Now;
+                    if (DateTime.Compare(args.CurrentTime, args.AlarmTime) >= 0 && isPause)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("闹钟响了!");
+                        Console.WriteLine();
+                        Console.WriteLine("输入任意符号关掉闹钟");
+
+                        if (Alarm != null)
+                        {
+                            Alarm(args);
+                        }
+
+                        if (Console.ReadLine() != null)
+                        {
+                            isPause = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"当前时间为 {DateTime.Now}");
+                        if (Tick != null)
+                        {
+                            Tick(args);
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var clockevent = new ClockEvent();
+            DateTime alarmTime = new DateTime();
+            int year, month, day, hour, minute, second;
+            Console.WriteLine("请设定时间");
+            Console.WriteLine("注意:");
+            Console.WriteLine("1. 按顺序输入年月日时分秒");
+            Console.WriteLine("2. 每输入一项，请按回车键");
+            Console.WriteLine("3. 如果不设定秒数，请输入0");
+            try
+            {
+                year = int.Parse(Console.ReadLine());
+                month = int.Parse(Console.ReadLine());
+                day = int.Parse(Console.ReadLine());
+                hour = int.Parse(Console.ReadLine());
+                minute = int.Parse(Console.ReadLine());
+                second = int.Parse(Console.ReadLine());
+                alarmTime = new DateTime(year, month, day, hour, minute, second);
+                Console.WriteLine($"你已将闹钟时间设定为 {alarmTime}");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("错误:超出范围");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("错误:格式有误");
+            }
+            finally
+            {
+                clockevent.ShowTime(alarmTime);
+            }
+        }
+
+    }
+}
---
+++ Homework4/Generic/Generic/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+
+            for (int x = 0; x < 10; x++)
+            {
+                intList.Add(x);
+            }
+            //逐步打印
+            intList.ForEach(x => Console.WriteLine(x));
+
+            //最大值
+            int max = int.MinValue;
+            intList.ForEach(x => { if (max < x) max = x; });
+            Console.WriteLine($"Max: {max}");
+
+            //最小值
+            int min = int.MaxValue;
+            intList.ForEach(x => { if (min > x) min = x; });
+            Console.WriteLine($"Min: {min}");
+
+            //总和
+            int sum = 0;
+            intList.ForEach(x => { sum += x; });
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T data { get; set; }
+
+            public Node(T t)
+            {
+                Next = null;
+                data = t;
+            }
+        }
+
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get
+                {
+                    return head;
+                }
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = tail.Next;
+                }
+            }
+
+            public void ForEach(Action<T> action)
+            {
+                for (Node<T> t = head; t != null; t = t.Next)
+                {
+                    action(t.data);
+                }
+            }
+        }
+    }
+}
+
