1811c8bcf0b3bbe2af6d2a67b8023340754530e4
SummatNG <79800346+SummatNG@users.noreply.github.com>
2021-04-28 10:37:54 +0800

    Add files via upload

---
+++ Homework1/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Calculator
+{
+
+
+    public class Calculator
+    {
+        public double GetResult(string symbol, double num1, double num2)
+        {
+            double result = 0;
+            if (symbol == "+")
+                result = num1 + num2;
+            else if (symbol == "-")
+                result = num1 - num2;
+            else if (symbol == "*")
+                result = num1 * num2;
+            else if (symbol == "/")
+                result = num1 / num2;
+            else
+                Console.WriteLine("请重新输入");
+            return result;
+
+        }
+
+    }
+
+}
---
+++ Homework1/Calculator/Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("请输入要计算的类型:");
+            string type = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("请输入第一个运算数:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入第二个运算数:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result = calculator.GetResult(type,num1,num2);
+            Console.WriteLine("计算结果为:" + result);
+        }
+
+    }
+}
